21656a9678d1728c78f5189e04d2b0b1f065912d
Henrique Sach Aguiar <henrique.sachtaguiar@ucalgary.ca>
2026-03-26 16:12:40 -0600

    Part3 completed

--- sweet-scoop-app/src/pages/FlavorsPage.js
+++ sweet-scoop-app/src/pages/FlavorsPage.js
@@ -4,11 +4,40 @@ import Footer from "../components/Footer";
 import FlavorCatalog from "../components/FlavorCatalog";
 import OrderList from "../components/OrderList";
 
-<div className="flavors-page"> 
+function FlavorsPage() {
+  const [order, setOrder] = useState(() => {
+    const saved = localStorage.getItem("order");
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  useEffect(() => {
+    const saved = localStorage.getItem("order", JSON.stringify(order));
+  }, [order]);
+
+  const handleAdd = (flavor) => {
+    setOrder((prevOrder) => {
+      const flavorExists = prevOrder.find((item) => item.id === flavor.id);
+
+      if (flavorExists) {
+        return prevOrder.map((item) =>
+          item.id === flavor.id
+            ? { ...item, quantity: item.quantity + 1 } : item,
+        );
+      }
+      return [...prevOrder, { ...flavor, quantity: 1 }];
+    });
+  };
+
+return (
+<div className="flavors-grid"> 
   <Header /> 
   <div className="content"> 
-    <FlavorCatalog /> 
-    <OrderList /> 
+    <FlavorCatalog onAdd={handleAdd} /> 
+    <OrderList order={order} setOrder={setOrder} /> 
   </div> 
   <Footer /> 
 </div>
+);
+}
+
+export default FlavorsPage;
